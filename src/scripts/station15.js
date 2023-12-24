95f8e19121ef49a1091da1b0992d762016241818
taku10101 <taku1011ht@gmail.com>
2023-12-24 19:36:40 +0900

    15 done

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,16 +1,42 @@
-async function getData() {
-  // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
-  // ３秒後にフルネームのリストを表示されるようにしましょう。
-  // 最初から表示されていると、クリアになりません。
-  const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+function getData() {
+  // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
+  // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
+  // そのため、JavaScriptで連結させて生成することにしました。
+  const test = [
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  //3000ミリ秒待つ
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(test.map(buildFullName));
+    }, 3000);
+  });
 }
 
-function test() {
-  return
+function buildFullName(data) {
+  // Station14の問題はこちらです。想定する答えをここを書いてください。
+  // 氏名がわかるようにしつつ、半角スペースで繋いでください。
+  // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  const fullName = data.family_name + " " + data.first_name;
+  data.full_name = fullName;
+  return data;
 }
 
+// 出力が想定される答え
+// const test = [
+//     {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affiliation: 'TechTrain', is_student: false},
+//     {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affiliation: 'HogeHoge大学', is_student: true}
+// ];
